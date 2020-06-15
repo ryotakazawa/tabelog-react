63b86234fd506607bcc314236c640abfc29e5a07
高沢崚 <ryotakazawaengineer@gmail.com>
2020-06-15 23:00:58 +0900

    get JSON from API bia axios

--- src/TopPage.js
+++ src/TopPage.js
@@ -3,30 +3,44 @@ import axios from 'axios';
 
 function TopPage() {
   const [data, setData] = useState({ hits: [] });
+  const [query, setQuery] = useState('ruby');
+  const [url, setUrl] = useState(
+      'http://hn.algolia.com/api/v1/search?query=ruby',
+  );
 
   useEffect(() => {
     const fetchData = async () => {
-    const result = await axios(
-       'http://hn.algolia.com/api/v1/search?query=redux',
-     );
+      const result = await axios(url);
 
-     setData(result.data);
-   };
+      setData(result.data);
+    };
 
     fetchData();
-  }, []);
+  }, [url]);
 
   return (
-    /*<>
-      <p>{data.total_hit_count}</p>
-    </>*/
-    <ul>
-      {data.hits.map(item => (
-        <li key={item.objectID}>
-          <a href={item.url}>{item.title}</a>
-        </li>
-      ))}
-    </ul>
+    <>
+      <input
+        type="text"
+        value={query}
+        onChange={event => setQuery(event.target.value)}
+      />
+      <button
+        type="button"
+        onClick={() =>
+          setUrl(`http://hn.algolia.com/api/v1/search?query=${query}`)
+        }
+      >
+        Search
+      </button>
+      <ul>
+        {data.hits.map(item => (
+          <li key={item.objectID}>
+            <a href={item.url}>{item.title}</a>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
